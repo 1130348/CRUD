b654f177a391365df0ea17d387017c5d590ac56f
1141041 <1141041@isep.ipp.pt>
2016-12-20 11:14:04 +0000

    Resolução de problemas no Package; Criação da ClassLibrary;

---
+++ ClassLibrary/ClassLibrary/Hashtag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lugares.Models
+{
+    public class Hashtag
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+
+        public virtual ICollection<POI> POIs { get; set; }
+    }
+}
---
+++ ClassLibrary/ClassLibrary/POI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lugares.Models
+{
+    public class POI
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public virtual ICollection<Hashtag> Hashtags { get; set; }
+    }
+}
