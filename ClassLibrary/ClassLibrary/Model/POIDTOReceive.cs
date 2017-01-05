0d0ea23da2b17536f96b3f7e5d1e5647aeec07b4
1141041 <1141041@isep.ipp.pt>
2017-01-05 16:51:20 +0000

    Criação da PortoVisitas. Inicialização do User.

---
+++ ClassLibrary/ClassLibrary/Model/POIDTOReceive.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Model
+{
+    public class POIDTOReceive : POIDTO
+    {
+        [ForeignKey("Local")]
+        public int LocalID { get; set; }
+        [ForeignKey("Categoria")]
+        public int CategoriaID { get; set; }
+        public POIDTOReceive(POI poi)
+        {
+            this.PoiID = poi.PoiID;
+            this.Nome = poi.Nome;
+            this.Descricao = poi.Descricao;
+            this.LocalID = poi.LocalID;
+            this.CategoriaID = poi.CategoriaID;
+        }
+    }
+}
